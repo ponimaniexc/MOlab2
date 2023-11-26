355ca9e426c42e6a26fafe76f4b32041b4b7cbc3
Vasily Avertsev <Basil.gagarin@outlook.com>
2023-11-26 18:24:10 +0500

    Добавил ограничения

--- MOlab2.cpp
+++ MOlab2.cpp
@@ -8,13 +8,21 @@ double Function(vector<double> u)
 	return 3 * u[1] * u[1] - 11 * u[0] - 3 * u[1] - u[2];
 }
 
-double g_1()
+double g_1(vector<double> u)
+{
+	return u[0] - 7 * u[1] + 3 * u[2] + 1;
+}
+
+double g_2(vector<double> u)
+{
+	return 5 * u[0] + 2 * u[1] - u[2] - 2;
+}
 
 int main()
 {
 	vector<double> x_0 = {1, 1, 1};
 	int m = 2, l = m;
-	double eps_1 = 1e-5, eps_2 = 1e-5;
+	double eps_1 = 1e-5, eps_2 = 1e-5, r = 10, C = 10;
 
 	cout << Function(x_0);
 
